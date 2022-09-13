5f81cadfcbf575f623882eac5216a95bf0109d01
BenCloughessy <110562848+BenCloughessy@users.noreply.github.com>
2022-09-13 10:56:56 -0400

    responsive to small screens

--- scripts.js
+++ scripts.js
@@ -345,9 +345,9 @@ const teenageChartMobile = new Chart(ctx4, {
               'rgb(245, 117, 66)',
               'rgb(204, 22, 22)',
             ],
-            radius: 115,
+            radius: 105,
             hoverOffset: 19,
-            offset: 15
+            offset: 12
           }]
     },
     
